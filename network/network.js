1dcec7480518aab98832f558e8ffaf51dd10c484
sun wei <1793786487@qq.com>
2020-03-01 00:21:17 +0800

    提取数据请求

---
+++ network/network.js
@@ -0,0 +1,28 @@
+
+const url="http://api.wukaka.com/"
+  
+//登录
+function login(data) {
+    return request({
+        url: url+"login",
+        method: 'post',
+        data: data
+    })
+  }
+
+
+//查看是否过期
+function life() {
+   return request({
+        url: url+"isLife",
+    })
+  }
+
+
+
+ //获取用户上次登录信息
+function get_user_info(username) {
+    return request({
+         url: url+"user/" + username + ""
+     })
+   }
